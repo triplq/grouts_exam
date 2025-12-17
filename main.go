c59945a43330cdbf8f1c5f0474909eb8d22aa573
Alexey Yaroshenko <alexy.yaroshenko05@icloud.com>
2025-12-17 22:43:05 +0300

    almost end

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"sync"
 )
-
-var wg sync.WaitGroup
 
 func gen(nums ...int) <-chan int {
 	ch := make(chan int)
@@ -18,6 +17,32 @@ func gen(nums ...int) <-chan int {
 	return ch
 }
 
-func main() {
+func worker(jobs <-chan int, results chan int, wg *sync.WaitGroup) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for j := range jobs {
+			results <- j * j
+		}
+	}()
+}
 
+func main() {
+	var wg sync.WaitGroup
+	Workers := 5
+	jobs := gen(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+	results := make(chan int, 5)
+
+	for range Workers {
+		worker(jobs, results, &wg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for res := range results {
+		fmt.Println(res)
+	}
 }
