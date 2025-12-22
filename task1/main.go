44f27e63a911ada907e032ab3cbd63a2aed26baf
Alexey Yaroshenko <alexy.yaroshenko05@icloud.com>
2025-12-22 13:52:07 +0300

    semaphor

--- task1/main.go
+++ task1/main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/triplq/goruts_exam/utils"
 )
 
-func worker(jobs <-chan int, results chan int, wg *sync.WaitGroup) {
+func worker(sem chan struct{}, jobs <-chan int, results chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	timer := time.NewTimer(1 * time.Second)
 	defer timer.Stop()
@@ -19,10 +19,13 @@ func worker(jobs <-chan int, results chan int, wg *sync.WaitGroup) {
 			if !ok {
 				return
 			}
+			sem <- struct{}{}
 			results <- val * val
+			<-sem
 			timer.Reset(1 * time.Second)
 		case <-timer.C:
 			fmt.Println(errors.New("times out"))
+			return
 		}
 	}
 }
@@ -32,11 +35,13 @@ func main() {
 	Workers := 5
 	wg.Add(Workers)
 
+	sem := make(chan struct{}, 3)
+
 	jobs := utils.Generator_by_slice(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
 	results := make(chan int, 5)
 
 	for range Workers {
-		go worker(jobs, results, &wg)
+		go worker(sem, jobs, results, &wg)
 	}
 
 	go func() {
